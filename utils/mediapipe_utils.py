5900607b1762e9bf9246a84dd98d9aa1aa4f6193
Kendymann <pmjk.tr3@gmail.com>
2025-02-06 11:55:23 -0800

    Set up project structure with initial files and dependencies

--- utils/mediapipe_utils.py
+++ utils/mediapipe_utils.py
@@ -1,2 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('OpenCV Feed', frame)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
 
 
