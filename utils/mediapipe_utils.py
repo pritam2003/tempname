a6ad16a371fe6f453440fb4247eb2119e712f988
Kendymann <pmjk.tr3@gmail.com>
2025-02-06 18:57:03 -0800

    FACE_CONNECTIONS no longer works in mediapipe so Changed to FACEMESH_CONTOURS could use TESSALATIONS for more details but for are use case i think Contours is fine

--- utils/mediapipe_utils.py
+++ utils/mediapipe_utils.py
@@ -22,7 +22,7 @@ def mediapipe_detection(image, model):
 # For the color and thickness can change to whatever we prefer
 def draw_styled_landmarks(image, results):
     # Draw face connections
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
                               mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), #Color the landmarks
                               mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)) #Color the connections
     # Draw pose connections
